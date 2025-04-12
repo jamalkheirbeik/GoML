72e5ffa172aea8cdd144094d3ce778035ee00c2b
jamalkheirbeik <jamalkheirbeik@gmail.com>
2025-04-12 21:33:29 +0300

    separate gates into their own directories

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-go build -o ./build/gates/and ./examples/gates/and/and.go
-go build -o ./build/gates/or ./examples/gates/or/or.go
-go build -o ./build/gates/xor ./examples/gates/xor/xor.go
+go build -o ./build/gates/and/and ./examples/gates/and/and.go
+go build -o ./build/gates/or/or ./examples/gates/or/or.go
+go build -o ./build/gates/xor/xor ./examples/gates/xor/xor.go
